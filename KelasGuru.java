7a07d210117f2b9860f273e2b4f27fc0e13f18ee
SuryaPutra <surya_yustitia_33rpl@student.smktelkom-mlg.sch.id>
2024-08-21 13:57:58 +0700

    Add files via upload

---
+++ KelasGuru.java
@@ -0,0 +1,13 @@
+
+
+public class KelasGuru {
+    public static void main(String[] args) {
+        TesGuru buPasha = new  TesGuru(286528, "bu Pasha", "Produktif RPL", "Tetap");
+        TesGuru buAulia = new  TesGuru(862637, "bu Aulia", "Produktif RPL", "Tetap");
+        TesGuru pakFalah = new  TesGuru(873892, "pak Falah", "Bahasa Indonesia", "Tetap");
+        TesGuru pakAndre = new  TesGuru(743963, "bu Andre", "Bahasa Inggris", "Tetap");
+        TesGuru buJevi = new  TesGuru(563784, "bu Jevi", "Bahasa Inggris", "Tetap");
+    
+    }
+    
+}
